593a4834dba0d678051e341749ddad6bc8482ab4
Timur Dzhalalov <timurdzhalalov@yahoo.com>
2023-03-06 01:25:01 +0100

    db parameters moved to config

--- src/config.py
+++ src/config.py
@@ -5,3 +5,14 @@ AUTH_EXPIRATION_SECS = 300
 """Seconds before auth key expires"""
 TG_TOKEN_ENV_NAME = "TG_TOKEN"
 """Telegram token env name"""
+
+#####################################
+# db.py
+#####################################
+DB_NAME = "DB_NAME"
+DB_USER = "DB_USER"
+DB_PASSWORD = "DB_PASSWORD"
+DB_HOST = "DB_HOST"
+DB_PORT = "DB_PORT"
+DB_HOST_DEFAULT = "0.0.0.0"
+DB_PORT_DEFAULT = "5432"
--- src/db.py
+++ src/db.py
@@ -2,6 +2,11 @@ import os
 import psycopg2
 from psycopg2.extensions import connection, cursor
 
+from config import (
+    DB_NAME, DB_USER, DB_PASSWORD,
+    DB_HOST, DB_PORT, DB_HOST_DEFAULT, DB_PORT_DEFAULT,
+)
+
 
 def get_db_conn() -> tuple[connection, cursor]:
     """Get Postgres connection and cursor
@@ -11,10 +16,10 @@ def get_db_conn() -> tuple[connection, cursor]:
         tuple[connection, cursor]
     """
     conn = psycopg2.connect(
-        database=os.environ.get("DB_NAME"),
-        user=os.environ.get("DB_USER"),
-        password=os.environ.get("DB_PASSWORD"),
-        host=os.environ.get("DB_HOST", "0.0.0.0"),
-        port=os.environ.get("DB_PORT", 5432),
+        database=os.environ.get(DB_NAME),
+        user=os.environ.get(DB_USER),
+        password=os.environ.get(DB_PASSWORD),
+        host=os.environ.get(DB_HOST, DB_HOST_DEFAULT),
+        port=os.environ.get(DB_PORT, DB_PORT_DEFAULT),
     )
     return conn, conn.cursor()
